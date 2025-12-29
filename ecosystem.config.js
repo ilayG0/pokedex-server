aa88d7b36c6c5824cb90637f9a63fe7bb9601659
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2025-12-29 13:38:08 +0200

    u

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,8 +18,13 @@ module.exports = {
       ref: "origin/main",
       repo: "git@github.com:ilayG0/moongodb-first-proj.git",
       path: "/home/ubuntu/pokedex-server",
+      ssh_options: "StrictHostKeyChecking=no",
+      "pre-deploy-local": "echo 'Deploying code to EC2'",
       "post-deploy":
-        `export NVM_DIR='$HOME/.nvm'; [ -s '$NVM_DIR/nvm.sh' ] && . '$NVM_DIR/nvm.sh'; npm install; pm2 startOrReload ecosystem.config.js --env production`,
+        `. /home/ubuntu/.nvm/nvm.sh; npm install; pm2 startOrReload ecosystem.config.js --env production`,
+      env: {
+        NODE_ENV: "production",
+      },
     },
   },
 };
