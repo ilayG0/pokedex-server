8b928e6a563115316d6d95a4b866b80bec0ef70b
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2025-12-29 12:05:52 +0200

    ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: "pokedex-server",
+      script: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: "ubuntu",
+      host: "my-ec2-dev", 
+      ref: "origin/main", 
+      repo: "git@github.com:ilayG0/moongodb-first-proj.git",
+      path: "/home/ubuntu/pokedex-server",
+      "post-deploy":
+        "npm install && pm2 startOrReload ecosystem.config.js --env production",
+    },
+  },
+};
