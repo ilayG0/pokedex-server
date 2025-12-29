5338d2b9415185845ed8fd7adf31befc6372ca4d
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2025-12-29 13:31:41 +0200

    change

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ function createApp() {
   app.use(express.json());
 
   app.get("/health", (req, res) => {
-    res.json({ status: "ok" });
+    res.json({ status: "ok" ,version: "2"});
   });
 
   app.use("/api/auth", authRoutes);
